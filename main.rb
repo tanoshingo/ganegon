03ce8eae5adc3517429185b8e4361a8e1fa8ad00
Tokumeikibou no Nakashimasan <rechngml@gmail.com>
2021-12-04 07:43:59 +0000

    テスト用プログラムをmain.rbに保存しました

--- main.rb
+++ main.rb
@@ -6,4 +6,14 @@ Window.bgcolor = C_BLACK
 
 Window.loop do
 	//ここに処理を書く
+	x = rand(Window.width)
+	y = rand(Window.height)
+	dx = dy = 2
+	Window.loop do
+		Window.draw_circle_fill(x, y, 10, C_RED)
+		dx = -dx if x < 0 || x > Window.width
+		dy = -dy if y < 0 || y > Window.height
+		x += dx
+		y += dy
+	end
 end
