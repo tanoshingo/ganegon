3840e00ccba5523094d8c883e28651117e26c75c
nakashima4 <j1822@matsue-ct.ac.jp>
2021-12-12 07:59:08 +0000

    阿鼻叫喚の地獄絵図

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@
 require 'dxopal';
 include DXOpal
 
-Image.register(:medic, "image/YAKU_cupcell.png")    #薬の画像
+Image.register(:medic, "image/REAL_YAKU.png")    #薬の画像
 Image.register(:enemy, "image/uirusu.png")  #ウイルスの画像
 
 #ウィンドウの初期設定
@@ -11,6 +11,78 @@ Window.height = 800
 Window.bgcolor = C_WHITE
 
 #クラス定義・メソッド定義-------------------------------------------------------------------------
+
+
+#medic用のクラス
+class Medic
+    
+    attr_accessor :x, :y, :dir, :color
+    
+    def initialize()
+    
+        #色
+        @RED = 1
+        @BLUE = 2
+        @YELLOW = 3
+        @color = rand(0..2)
+        
+        #向き（左1、上2、右3、下4）
+        @dir
+        
+        #くっついているかどうか
+        @connecting = 1
+        
+        #どの状態か（queue内部 or 操作中 or 設置済み）
+        @QUEUEING = 0
+        @PLAYING = 1
+        @PUTTED = 2
+        @mode = @QUEUEING
+        
+        @x = -10
+        @y = -10
+        
+        @medics = Image[:medic].slice_tiles(3, 2)    #薬の画像を分割（配列化、medics[0]～medics[5]）
+    end
+        
+    def draw() 
+        
+        @mapchip_size = 34
+        if (@mode == @QUEUEING && @dir == 2) then
+            Window.draw(900, 100, @medics[@color])
+        elsif(@mode == @QUEUEING && @dir == 4)
+            Window.draw(934, 100, @medics[@color])
+        else
+            Window.draw(x * @mapchip_size + @default_x, y * @mapchip_size + @default_y, @empty_chip)
+        end
+    end
+end
+
+#player用のクラス
+class Player < Medic
+    
+    def initialize(queue)
+        
+        #色
+        @RED = 1
+        @BLUE = 2
+        @YELLOW = 3
+        
+        #どの状態か（queue内部 or 操作中 or 設置済み）
+        @QUEUEING = 0
+        @PLAYING = 1
+        @PUTTED = 2
+        
+        #座標
+        @mymedic_x = [4, 5]
+        @mymedic_y = [1, 1]
+        
+        #薬の情報
+        @mymedic_color = queue.color
+        @mymedic_dir = queue.dir
+        @mymedic_connecting = 1
+        @mode = @PLAYING
+    end
+end
 
 #概　要：field用のクラス
 class Field
@@ -29,7 +101,7 @@ class Field
         @map = Array.new(@FIELD_Y) { Array.new(@FIELD_X, @EMPTY) }
         
         #画像データを格納する変数
-        @medics = Image[:medic].slice_tiles(6, 1)    #薬の画像を分割（配列化、medics[0]～medics[5]）
+        @medics = Image[:medic].slice_tiles(3, 2)    #薬の画像を分割（配列化、medics[0]～medics[5]）
         @virus = Image[:enemy].slice_tiles(2, 2) #ウイルスの画像を分割（配列化、virus[0]～virus[2]）
         @bottle_chip = Image.new(34, 34, [125, 125, 125]) #薬瓶のbit画像を生成
         @empty_chip = Image.new(34, 34, [0, 0, 0]) #薬瓶のbit画像を生成
@@ -84,15 +156,33 @@ class Field
     end
 end
 
+
 #メイン部分--------------------------------------------------------------------------------------
 Window.load_resources do    #画像変数などの定義はここでする
     
+    queue = Array.new(2)
+    queue[0] = Medic.new    #待機中の薬を作成
+    queue[1] = Medic.new    #待機中の薬を作成
+    queue[0].dir = 2    #右向き
+    queue[1].dir = 4    #左向き
     field = Field.new   #mapを作成する
+    p_medic = Array.new(2)
+    p_medic[0] = Player.new(queue[0])    #プレイヤー用の薬を作成
+    p_medic[1] = Player.new(queue[1])    #プレイヤー用の薬を作成
     
     #ここにゲーム全体のループ処理を記述
     Window.loop do
         
+        queue[0] = Medic.new
+        queue[1] = Medic.new
+        queue[0].dir = 2    #右向き
+        queue[1].dir = 4    #左向き
         field.draw()
+        queue[0].draw()
+        queue[1].draw()
+        p_medic[0].draw()
+        p_medic[1].draw()
+        sleep 0.05
         
     end
 end
